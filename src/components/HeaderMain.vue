2884d22b90792bc26abc5879dd63769ede9e4526
dariuschen <1323808021@qq.com>
2021-05-16 19:49:29 +0800

    feat: element和vue-router引入--story=20210516

---
+++ src/components/HeaderMain.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    请先登录
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderMain',
+  data () {
+    return {
+      input: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
